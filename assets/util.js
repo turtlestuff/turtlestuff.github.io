b09b0664f0de97e8e4a4ba309c5993d48f91d66e
Vrabbers <nininininimocp@gmail.com>
2019-08-22 18:04:27 -0300

    very COOL thing

---
+++ assets/util.js
@@ -0,0 +1,6 @@
+function nav(){
+    document.write(`
+    <button onclick="window.location.href='index.html'">Home</button>
+    <button onclick="window.location.href='leisure.html'">Leisure.NET</button>`)
+
+}
