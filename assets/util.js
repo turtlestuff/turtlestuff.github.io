77f7b9994115316d528d66b6b1d9e2e1a3faa630
Vrabbers <nininininimocp@gmail.com>
2019-11-30 10:31:30 -0300

    Entertaining Drawing Page

--- assets/util.js
+++ assets/util.js
@@ -1,6 +1,7 @@
 function nav(){
     document.write(`
     <button onclick="window.location.href='index.html'">Home</button>
-    <button onclick="window.location.href='leisure.html'">Leisure.NET</button>`)
+    <button onclick="window.location.href='leisure.html'">Leisure.NET</button>
+    <button onclick="window.location.href='entdrawing.html'">Entertaining Drawing</button>`)
 
 }
